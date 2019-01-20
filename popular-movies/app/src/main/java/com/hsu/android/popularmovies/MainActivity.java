0d0cb92fcd68ec53c5a054ab58c6cbb9ac06d038
jessica <hsujessica21@gmail.com>
2019-01-20 14:13:31 -0600

    add starting code to movie posters in grid layout

--- popular-movies/app/src/main/java/com/hsu/android/popularmovies/MainActivity.java
+++ popular-movies/app/src/main/java/com/hsu/android/popularmovies/MainActivity.java
@@ -2,12 +2,38 @@ package com.hsu.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
 
+/**
+ * Main page with all the movie posters
+ * Will call IMDB API for most popular or highest rating
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static GridView moviePostersGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // on page load, show most popular movies
+    }
+
+    /**
+     * Call Top 10 Rated Movies
+     * https://api.themoviedb.org/3/movie/top_rated?api_key=73c51e9ec420508456d018916725686d&language=en-US&page=10
+     */
+    public void getHighestRatingMovies(View view) {
+
+    }
+
+    /**
+     * Call Top 10 Most Popular Movies
+     * https://api.themoviedb.org/3/movie/popular?api_key=73c51e9ec420508456d018916725686d&language=en-US&page=10
+     */
+    public void getMostPopularMovies(View view) {
+
     }
 }
---
+++ popular-movies/app/src/main/java/com/hsu/android/popularmovies/MovieDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.hsu.android.popularmovies;
+
+/**
+ * Display movie details on a separate pop up page when you tap on movie poster.
+ * Details include original title, movie poster image thumbnail, plot synopsis (overview),
+ * user rating (vote_average), release date
+ * Will call IMDB API for movie details
+ */
+public class MovieDetailsActivity {
+
+    /**
+     * Get movie details
+     * https://api.themoviedb.org/3/movie/{movie_id}?api_key=73c51e9ec420508456d018916725686d&language=en-US
+     */
+}
