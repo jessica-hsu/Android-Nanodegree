fa26c9f9ee6eb05e9e2852c3ec64a6413accd84b
jessica <hsujessica21@gmail.com>
2019-02-03 12:52:12 -0600

    [NetworkUtils.java] update class with methods

--- popular-movies-app/app/src/main/java/com/udacity/android/app/utils/NetworkUtils.java
+++ popular-movies-app/app/src/main/java/com/udacity/android/app/utils/NetworkUtils.java
@@ -12,4 +12,60 @@ import java.util.Properties;
  * Helper class with methods to call external API
  */
 public class NetworkUtils {
+    private static final String IMDB_URL = "https://api.themoviedb.org/3/movie/";
+    private static final String LANGUAGE = "en-US";
+    private static final String LIMIT = "10";
+    private static String KEY = null;
+
+    /**
+     * Build endpoint with given params
+     * @param api
+     * @return
+     */
+    public static URL buildEndpoint(String api) {
+
+        // read config file for API KEY
+        Properties prop = new Properties();
+        try {
+            InputStream in = NetworkUtils.class.getClassLoader().getResourceAsStream("config.properties");
+            prop.load(in);
+            KEY = prop.getProperty("key");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // create uri
+        Uri uri = Uri.parse(IMDB_URL+api).buildUpon()
+                    .appendQueryParameter("key", KEY)
+                    .appendQueryParameter("language", LANGUAGE)
+                    .appendQueryParameter("page", LIMIT)
+                    .build();
+
+        URL endpoint = null;
+        try {
+            endpoint = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return endpoint;
+
+    }
+
+    /**
+     * Get Http Response from IMDB
+     */
+    public static String getHttpResponse(URL url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String current=null;
+        StringBuilder sb = new StringBuilder();
+        while ((current = br.readLine()) != null) {
+            sb.append(current+"\n");
+        }
+        br.close();
+        conn.disconnect();
+        return sb.toString();
+    }
 }
