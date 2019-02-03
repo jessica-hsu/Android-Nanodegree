695fe617972b1219ac29d0ea170b40e06f55bdf4
jessica <hsujessica21@gmail.com>
2019-02-03 13:32:18 -0600

    [Movie.java] update class with attributes and getters

--- popular-movies-app/app/src/main/java/com/udacity/android/app/model/Movie.java
+++ popular-movies-app/app/src/main/java/com/udacity/android/app/model/Movie.java
@@ -4,4 +4,44 @@ package com.udacity.android.app.model;
  * Movie object with attributes like movie_id, plot, release date, poster, etc
  */
 public class Movie {
+
+    private String poster;
+    private String id;
+    private String title;
+    private String date;
+    private String plot;
+    private String rating;
+
+    public Movie(String id, String title, String poster, String plot, String date, String rating) {
+        this.poster = poster;
+        this.id = id;
+        this.plot = plot;
+        this.date = date;
+        this.rating = rating;
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return this.poster;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getPlot() {
+        return this.plot;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
 }
