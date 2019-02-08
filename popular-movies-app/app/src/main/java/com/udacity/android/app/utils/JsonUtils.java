9b0a0d2af9993b06a8c9e14d997b9a3bcdf23f31
jessica <hsujessica21@gmail.com>
2019-02-07 20:04:37 -0600

    write helper class to parse json string to objects

--- popular-movies-app/app/src/main/java/com/udacity/android/app/utils/JsonUtils.java
+++ popular-movies-app/app/src/main/java/com/udacity/android/app/utils/JsonUtils.java
@@ -1,8 +1,35 @@
 package com.udacity.android.app.utils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.udacity.android.app.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Helper class with methods to convert httpresponse json string
  * to JSON object, then convert to List<Movie>
  */
 public class JsonUtils {
+    public static List<Movie> parse(String details) throws JSONException {
+
+        List<Movie> movies = new ArrayList<Movie>();
+        JSONObject mainObj = new JSONObject(details);
+        JSONObject[] results = (JSONObject[]) mainObj.get("results");
+        for (int i = 0; i < results.length; i++) {
+            String id = results[i].getString("id");
+            String original_title = results[i].getString("original_title");
+            String poster_path = results[i].getString("poster_path");
+            String overview = results[i].getString("overview");
+            String vote_average = results[i].getString("vote_average");
+            String release_date = results[i].getString("release_date");
+            Movie m = new Movie(id, original_title, poster_path, overview, release_date, vote_average);
+            movies.add(m);
+        }
+
+        return movies;
+    }
 }
