3616767632fa9f891305151d31cc9bea92025741
jessica <hsujessica21@gmail.com>
2019-02-23 17:36:45 -0600

    initial commit

---
+++ popular-movies-app-2/app/src/main/java/com/udacity/android/app/ImageAdapter.java
@@ -0,0 +1,78 @@
+package com.udacity.android.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.udacity.android.app.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<Movie> movies;
+
+    public ImageAdapter(Context c) {
+        this.mContext = c;
+
+    }
+
+    public int getCount() {
+        return movies.size();
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void setMovieDetails(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            //imageView.setLayoutParams(new ViewGroup.LayoutParams(185, 185));
+           // imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+           // imageView.setPadding(0, 10, 0, 10);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        // load images in
+        String imageUrl = "http://image.tmdb.org/t/p/w185/" + movies.get(position).getPoster();
+        Picasso.get().load(imageUrl).into(imageView);
+
+        //set onclick
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MovieDetailsActivity.class);
+                intent.putExtra("title", movies.get(position).getTitle());
+                intent.putExtra("poster", movies.get(position).getPoster());
+                intent.putExtra("plot", movies.get(position).getPlot());
+                intent.putExtra("rating", movies.get(position).getRating());
+                intent.putExtra("date", movies.get(position).getDate());
+                mContext.startActivity(intent);
+            }
+        });
+        return imageView;
+    }
+
+
+
+}
---
+++ popular-movies-app-2/app/src/main/java/com/udacity/android/app/MovieDetailsActivity.java
@@ -0,0 +1,53 @@
+package com.udacity.android.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Activity to populate view with movie details
+ */
+public class MovieDetailsActivity extends Activity {
+
+    private static ImageView image;
+    private static TextView title;
+    private static TextView plot;
+    private static TextView date;
+    private static TextView ratings;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        image = (ImageView) findViewById(R.id.selected_movie);
+        title = (TextView) findViewById(R.id.title_tv);
+        plot = (TextView) findViewById(R.id.plot_tv);
+        date = (TextView) findViewById(R.id.date_tv);
+        ratings = (TextView) findViewById(R.id.ratings_tv);
+
+        populateDetails();
+    }
+
+    private void populateDetails() {
+        String imageUrl = "http://image.tmdb.org/t/p/w185/" + getIntent().getStringExtra("poster");
+        Picasso.get().load(imageUrl).into(image);
+        title.setText(getIntent().getStringExtra("title"));
+        plot.setText(getIntent().getStringExtra("plot"));
+        date.setText(getIntent().getStringExtra("date"));
+        String rating_txt = getIntent().getStringExtra("rating")+"/10";
+        ratings.setText(rating_txt);
+
+        /*example youtube link: https://www.youtube.com/watch?v=ii3n7hYQOl4
+        https://www.youtube.com/watch?v={key}
+        https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={api_key}&language=en-US*/
+
+
+        /*reviews:
+        https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key={api_key}&language=en-US&page=1
+         */
+
+    }
+}
