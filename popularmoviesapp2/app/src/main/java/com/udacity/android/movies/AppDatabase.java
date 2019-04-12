e0c1c120f919b5d93fddcdbf89f57c516fb19bdf
jessica <hsujessica21@gmail.com>
2019-04-11 21:39:20 -0500

    Update AppDatabase with extend RoomDatabase

--- popularmoviesapp2/app/src/main/java/com/udacity/android/movies/AppDatabase.java
+++ popularmoviesapp2/app/src/main/java/com/udacity/android/movies/AppDatabase.java
@@ -1,11 +1,12 @@
 package com.udacity.android.movies;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
 
 import com.udacity.android.movies.dao.MovieDao;
 import com.udacity.android.movies.entity.Movie;
 
 @Database(entities = {Movie.class}, version = 1)
-public abstract class AppDatabase {
+public abstract class AppDatabase extends RoomDatabase {
     public abstract MovieDao movieDao();
 }
