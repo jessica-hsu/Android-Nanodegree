f132920b76409401161f33d9ea054965ef5fd235
jessica <hsujessica21@gmail.com>
2019-08-01 21:31:04 -0500

    finish search page ui

--- Capstone_Stage2/app/src/main/java/com/udacity/android/podcastbemine/ui/SearchActivity.java
+++ Capstone_Stage2/app/src/main/java/com/udacity/android/podcastbemine/ui/SearchActivity.java
@@ -1,16 +1,17 @@
-package com.udacity.android.podcastbemine;
+package com.udacity.android.podcastbemine.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.udacity.android.podcastbemine.R;
+
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.getSupportActionBar().hide();
         setContentView(R.layout.activity_search);
         createSpinner();
     }
