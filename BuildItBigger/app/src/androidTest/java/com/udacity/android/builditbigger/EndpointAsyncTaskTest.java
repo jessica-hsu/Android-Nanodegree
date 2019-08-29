cc2636c05bc4a8e9910c3b143061831100386164
jessica <hsujessica21@gmail.com>
2019-08-28 22:20:48 -0500

    make async task test

--- BuildItBigger/app/src/androidTest/java/com/udacity/android/builditbigger/EndpointAsyncTaskTest.java
+++ BuildItBigger/app/src/androidTest/java/com/udacity/android/builditbigger/EndpointAsyncTaskTest.java
@@ -1,4 +1,30 @@
 package com.udacity.android.builditbigger;
 
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
 public class EndpointAsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testAsyncTask() {
+
+        try {
+            EndpointAsyncTask task = new EndpointAsyncTask();
+            String myJoke = task.execute().get();
+            assertNotNull(myJoke);
+        } catch (Exception e) {
+            Log.e("TestAsyncEndpoint", e.getStackTrace().toString());
+        }
+
+    }
 }
